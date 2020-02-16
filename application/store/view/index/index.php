1279e7de31c578443d64b1261e7ae51ac1e10cf7
kyang <vectormail@163.com>
2020-02-16 19:20:04 +0800

    ff

--- application/store/view/index/index.php
+++ application/store/view/index/index.php
@@ -3,7 +3,7 @@
         <div class="widget-body">
             <div class="tpl-page-state am-margin-top-xl">
                 <div class="tpl-page-state-title am-text-center"><?= $setting['store']['values']['name'] ?></div>
-                <div class="tpl-error-title-info">Welcome To YoShop System</div>
+                <div class="tpl-error-title-info">Welcome To Kyang Mini-Program System</div>
                 <div class="tpl-page-state-content tpl-error-content">
                     <p>商城管理后台</p>
                 </div>
