ca5bc6e91572997a3a0a5a221362b0058718fb71
jack <2098156794@qq.com>
2020-02-19 12:11:29 +0800

    积分商城model

---
+++ application/common/model/ShopModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huaidao
+ * Date: 2020/2/19
+ * Time: 0:37
+ */
+
+namespace app\common\model;
+
+
+class ShopModel extends BaseModel
+{
+    protected $name = 'score_shop';
+}
