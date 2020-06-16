068065dbaa1dd1aaec70d4a5dec82c848b98d265
kyang <yk1106>
2020-06-16 13:07:36 +0800

    时间下架

--- public/test.php
+++ public/test.php
@@ -1,2 +1,9 @@
 <?php
+$array = scandir('F:\phpstudy_pro\WWW\www.dd-front.com\wxcomponents\vant\dist');
 
+$l = [];
+foreach ($array as $v){
+    if ($v == '.' || $v == '..') continue;
+
+    echo '"van-'.$v.'": "/wxcomponents/vant/dist/'.$v.'/index",'.PHP_EOL;
+}
